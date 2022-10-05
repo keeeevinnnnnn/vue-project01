c2ba6053f8fdf8d2888f392c746487c5a3dbbe9c
keeeevinnnnnn <kevin85627@gmail.com>
2022-10-05 16:18:54 +0800

    123

--- src/components/BigImg.vue
+++ src/components/BigImg.vue
@@ -33,7 +33,7 @@ export default {
   z-index: 9999;
   top: 0;
   left: 0;
-  background-color: azure;
+  background-color: rgba(20, 20, 20, 0.13);
   width: 100%;
   height: 100%;
   overflow: hidden;
